324e97015ed2f168f1ea3756d7463df6f5ac11b4
QIHUAN1 <qjiang1@inside.artcenter.edu>
2023-04-19 02:16:27 -0700

    1

--- Assets/Script/ShowSign.cs
+++ Assets/Script/ShowSign.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickBook : MonoBehaviour
+public class ShowSign : MonoBehaviour
 {
-
     public GameObject sign;
     public bool isInteracted;
-
-    public GameObject pickSign;
 
     // Start is called before the first frame update
     void Start()
     {
         sign.SetActive(false);
         isInteracted = false;
-
-        pickSign.SetActive(false);
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -30,14 +26,7 @@ public class pickBook : MonoBehaviour
         {
             sign.SetActive(false);
         }
-
-        if (isInteracted == true && Input.GetKeyDown(KeyCode.F))
-        {
-            gameObject.SetActive(false);
-            pickSign.SetActive(true);
-        }
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
